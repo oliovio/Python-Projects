239caf9cd7f52ea6c0130d5849773546206713e2
Olivier Roberge <31775707+oliovio@users.noreply.github.com>
2024-02-04 21:51:32 -0500

    Final Version
    Quiz Game Finished

--- quiz-game.py
+++ quiz-game.py
@@ -1,13 +1,38 @@
 print("Welcome to my quiz game!")
 
-playing = input("Do you want to play?  ")
+playing = input("Do you want to play?  ").lower()
 print(playing)
-if playing.lower() != "yes":
+if playing != "yes":
     print("Okay! Goodbye!")
     quit()
 print("Okay! Let's play :)")
-answer = input("What does CPU stand for?  ")
+score = 0
+
+answer = input("What does CPU stand for?  ").lower()
 if answer == "central processing unit":
     print("Correct!")
-else: answer != "central processing unit"
-print("Incorrect!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+answer = input("What does GPU stand for?  ").lower()
+if answer == "graphing processing unit":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+
+answer = input("What does RAM stand for?  ").lower()
+if answer == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for?  ").lower()
+if answer == "power supply unit":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect!")
+print("You got " + str(score) + " questions correct!")
